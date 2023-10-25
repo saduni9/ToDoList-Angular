0715824cbef519a4ad88831d4c4c1c52f1f367a7
saduni9 <rasikalahms.20@uom.lk>
2023-10-25 21:02:59 +0530

    All parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import { TodoComponent } from './todo/todo.component';
 
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +28,15 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    DragDropModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
